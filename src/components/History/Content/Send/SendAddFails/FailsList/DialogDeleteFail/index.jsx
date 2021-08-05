8b80e82393c3215b605da176c2805170e58027a6
Muhammad <timurkaevmuhammad4@gmail.com>
2021-08-05 17:21:39 +0300

    added dialog of delete file

---
+++ src/components/History/Content/Send/SendAddFails/FailsList/DialogDeleteFail/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { deleteOneFail } from '../../../../../../../redux/ducks/files';
+
+import style from './style.module.css';
+
+function DialogDeleteFail({ open, handleClose }) {
+  const dispatch = useDispatch()
+
+  const handleDeleteFail = () => {
+    dispatch(deleteOneFail(open.id, open.format, open.amount, open.groupId));
+    handleClose();
+  }
+
+
+  return (
+    <div className={style['dialog-remove']}>
+      <div className={style.title}>Вы уверены</div>
+      <div className={style.buttons}>
+        <div onClick={handleDeleteFail} className={style.button}>
+          Да
+        </div>
+        <div onClick={handleClose} className={style.button}>
+          Нет
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default DialogDeleteFail
--- src/components/History/Content/Send/SendAddFails/FailsList/index.jsx
+++ src/components/History/Content/Send/SendAddFails/FailsList/index.jsx
@@ -1,40 +1,62 @@
+import React, { useState } from 'react';
 import ListItemDocument from './ListItemDocument';
 import ListItemImage from './ListItemImage';
 import ListItemVideo from './ListItemVideo';
 import ListItemAudio from './ListItemAudio';
-import { useDispatch } from 'react-redux';
-import { deleteOneFail } from '../../../../../../redux/ducks/files';
+import Dialog from '../../../../General/Dialog'
 
 import style from './style.module.css';
-
-
-
+import DialogDeleteFail from './DialogDeleteFail';
 
 function FailsList(props) {
-  
-  const dispatch = useDispatch()
+  const [open, setOpen] = useState({
+    open: false,
+    id: "",
+    format: "",
+    amount: "",
+    groupId: "",
+  });
 
-  const handleDeleteFail = (id, format, amount, groupId = "") => {
-    dispatch(deleteOneFail(id, format, amount, groupId));
+  const handleOpen = (id, format, amount, groupId) => {
+    setOpen({
+      open: true,
+      id,
+      format,
+      amount,
+      groupId,
+    })
   }
 
-  
+  const handleClose = () => {
+    setOpen({
+      open: false,
+      id: "",
+      format: "",
+      amount: "",
+      groupId: "",
+    })
+  }
 
   return (
-    <ul className={`${style.fails__list} ${props.failAdd ? '' : ''}`}>
+    <React.Fragment>
+      <ul className={`${style.fails__list} ${props.failAdd ? '' : ''}`}>
       {props.image.length === 0 && props.images.length === 0 ?
-        null : <ListItemImage handleDeleteFail={handleDeleteFail} image={props.image} images={props.images} />
+        null : <ListItemImage handleDeleteFail={handleOpen} image={props.image} images={props.images} />
       }
       {props.application.length === 0 && props.applications.length === 0 ?
-        null : <ListItemDocument handleDeleteFail={handleDeleteFail} application={props.application} applications={props.applications} />
+        null : <ListItemDocument handleDeleteFail={handleOpen} application={props.application} applications={props.applications} />
       }
       {props.video.length === 0 && props.videos.length === 0 ?
-        null : <ListItemVideo handleDeleteFail={handleDeleteFail} video={props.video} videos={props.videos} />
+        null : <ListItemVideo handleDeleteFail={handleOpen} video={props.video} videos={props.videos} />
       }
       {props.audio.length === 0 && props.audios.length === 0 ?
-        null : <ListItemAudio handleDeleteFail={handleDeleteFail} audio={props.audio} audios={props.audios} />
+        null : <ListItemAudio handleDeleteFail={handleOpen} audio={props.audio} audios={props.audios} />
       }
     </ul>
+    <Dialog handleClose={handleClose} open={open.open}>
+      <DialogDeleteFail  handleClose={handleClose} open={open} />
+    </Dialog>
+    </React.Fragment>
   );
 }
 
